c4cb8fe215c2f327d37952505755195f3d25b507
Mouad <mouadelfaiz@outlook.com>
2023-09-05 20:11:52 -0400

    make components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const App_Id = "9acc583b";
-  const App_Key = "6d7fd05f3d9c2308e3d3e79bf41d0393";
-  const response = await fetch(``)
   return (
    <div className='App'>
     <h1>Hello World</h1>
