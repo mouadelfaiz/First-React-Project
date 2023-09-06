9242601bd9738455f9610b648690d73ba34dd2ff
Mouad <mouadelfaiz@outlook.com>
2023-09-06 13:04:53 -0400

    adding pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-   <div className='App'>
-    <h1>Hello World</h1>
-   </div>
-  )
+    <div className="App">
+      <Navbar />
+
+      <div className="routes">
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/pokelist" element={<Pokelist />} />
+          <Route exact path="/pokedetails" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/PokemonList/PokemonList.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PokemonList() {
-  return (
-    <div>PokemonList</div>
-  )
-}
