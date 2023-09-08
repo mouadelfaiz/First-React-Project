5748e92844cc136fb2f3baf3a6a87f49b4188761
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-09-08 14:35:58 -0400

    adding Custume link to NavBar
    check if the click page match the path and set it as active

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "../Styles/Navbr.css"
-import { Link } from "react-router-dom";
+import React, { Children } from "react";
+import "../Styles/Navbr.css";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function NavBar() {
   return (
@@ -9,16 +9,23 @@ export default function NavBar() {
         Home
       </Link>
       <ul>
-        <li>
-          <Link to="/pokemonlist">Pokemon Card</Link>
-        </li>
-        <li>
-          <Link to="/pokedetails">Pokemon Details</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
+        <CustomLink to="/pokemonlist">Pokemon Card</CustomLink>
+        <CustomLink to="/pokedetails">Pokemon Details</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
       </ul>
     </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+   const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({path: resolvedPath.pathname, end: true})
+  // using end to make sure the hole path is indenticla
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
