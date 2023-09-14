b08a4476cb4db56efb76813354a0bf2f6e26cb72
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-09-14 07:36:02 -0400

    comment changed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,7 +19,7 @@ export default function NavBar() {
 function CustomLink({ to, children, ...props }) {
    const resolvedPath = useResolvedPath(to)
   const isActive = useMatch({path: resolvedPath.pathname, end: true})
-  // using end to make sure the hole path is indenticle
+  // using end to make sure the whole path is identicl
   return (
     <li className={isActive ? "active" : ""}>
       <Link to={to} {...props}>
